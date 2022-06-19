f65f3cfe60548af1754601cad3ac8d45696adb95
ndkim1997 <kim.nick7@gmail.com>
2022-06-18 23:35:05 -0700

    add api key and basic setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import axios from 'axios';
+
+function App() {
+
+    const url = 'http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=f6c53c759d4be39c25c1cf5a053f8ecd'
+
+    return (
+        <div className="App">
+
+        </div>
+    );
+}
+
+export default App;
